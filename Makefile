d48aef08c31c00a47dfb6ed4bdc3a6c741ebd42b
Martin Fracker <martin.frackerjr@gmail.com>
2017-04-24 15:03:43 -0500

    Complete bonus problem

--- Makefile
+++ Makefile
@@ -1,11 +1,19 @@
-all: p1.jar
+all: p1.jar p2.jar
 
-run-p1-dev: p1.jar
-	hadoop jar p1.jar TwitterDataMinerP1 input.txt output
+run-p1: p1.jar
+	hadoop jar $< TwitterDataMinerP1 input.txt output
+
+run-p2: p2.jar
+	hadoop jar $< TwitterDataMinerP2 input.txt output
 
 p1.jar: TwitterDataMinerP1.java
-	hadoop com.sun.tools.javac.Main TwitterDataMinerP1.java
-	jar cf p1.jar TwitterDataMinerP1*.class
+	hadoop com.sun.tools.javac.Main $<
+	jar cf $@ TwitterDataMinerP1*.class
+
+p2.jar: TwitterDataMinerP2.java
+	hadoop com.sun.tools.javac.Main $<
+	jar cf $@ TwitterDataMinerP2*.class
+
 
 clean:
 	rm *.jar *.class
---
+++ TwitterDataMinerP2.java
@@ -0,0 +1,98 @@
+import java.io.IOException;
+import java.util.Iterator;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.lang.StringBuilder;
+import java.lang.Iterable;
+import java.util.List;
+import java.util.Arrays;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+// started with the word count class from 
+// https://hadoop.apache.org/docs/current/hadoop-mapreduce-client/hadoop-mapreduce-client-core/MapReduceTutorial.html
+public class TwitterDataMinerP2 {
+    public static class TokenizerMapper
+        extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context
+                        ) throws IOException, InterruptedException {
+            List<String> lines = Arrays.asList(value.toString().split("\n"));
+            Iterator<String> lineItr = lines.iterator();
+            boolean foundSleep = false;
+            boolean readingContent = false;
+            String hour;
+            while(lineItr.hasNext() && !foundSleep) {
+                String line = lineItr.next();
+                if (line.length() < 1)
+                    continue;
+                if (line.charAt(0) == 'T' && !readingContent) {
+                    String[] time = line.split("\\s+");
+                    hour = time[2].substring(0, 2); // just get the hour
+                    word.set(hour);
+                }
+                if (line.charAt(0) == 'W') {
+                    readingContent = true;
+                }
+                if (readingContent) {
+                    List<String> words = Arrays.asList(line.split("\\s+"));
+                    if (words.contains("sleep")) {
+                        foundSleep = true;
+                    }
+                }
+            }
+            value.set(key.toString());
+            if (foundSleep) {
+                context.write(word, one);
+            }
+        }
+    }
+
+    // same reducer from word count example
+    public static class IntSumReducer
+        extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+                           ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    // same main from word count example
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        conf.set("textinputformat.record.delimiter", "\n\n");
+        Job job = Job.getInstance(conf, "word count");
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setJarByClass(TwitterDataMinerP2.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
